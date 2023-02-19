d26b3f836b8933c1858ef6b7da22ffecf2cf26fc
huynhutluong <huyb1910073@student.ctu.edu.vn>
2023-02-19 20:05:51 +0700

    Sua mot so loi, hoan thanh lab 1

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -1,27 +1,27 @@
 exports.create = (req, res) => {
-    res.send({message: "create handler"})
+    res.send({ message: "create handler" });
 };
 
 exports.findAll = (req, res) => {
-    res.send({message: "findAll handler"})
+    res.send({ message: "findAll handler" });
 };
 
 exports.findOne = (req, res) => {
-    res.send({message: "findOne handler"})
+    res.send({ message: "findOne handler" });
 };
 
 exports.update = (req, res) => {
-    res.send({message: "update handler"})
+    res.send({ message: "update handler" });
 };
 
 exports.delete = (req, res) => {
-    res.send({message: "delete handler"})
+    res.send({ message: "delete handler" });
 };
 
 exports.deleteAll = (req, res) => {
-    res.send({message: "deleteAll handler"})
+    res.send({ message: "deleteAll handler" });
 };
 
 exports.findAllFavourite = (req, res) => {
-    res.send({message: "findAllFavourite handler"})
+    res.send({ message: "findAllFavourite handler" });
 };
